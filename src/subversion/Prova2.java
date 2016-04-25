eecbe0a87a24406871413bc555a351ed7ccaf3fd
RogerLera <RogerLera93@gmail.com>
2016-04-25 16:30:33 +0200

    Afegit Prova2

---
+++ src/subversion/Prova2.java
@@ -0,0 +1,15 @@
+package subversion;
+
+
+/**
+ * Classe Prova2.
+ *
+ * @author Roger Lera
+ * @version Abril 2016
+ */
+public class Prova2 {
+
+	public static void main(String[] args) {
+		System.out.println("Prova2.");
+	}
+}
